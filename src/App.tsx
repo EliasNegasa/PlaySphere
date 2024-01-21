b31337d7a5b4e21f0504df6a5629fa6a14f50b5b
= <=>
2024-01-21 21:27:53 +0300

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
-      <p>PlaySphere</p>
-      <Button colorScheme="blue">Button</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"header" "main"`,
+        lg: `"header header" "nav main"`,
+      }}
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="orange.300" area={'header'}>
+        Header
+      </GridItem>
+      <Show above='lg'>
+        <GridItem pl="2" bg="pink.300" area={'nav'}>
+          Nav
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area={'main'}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
