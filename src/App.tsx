fb51a3c7ae3ce5a32c9f6cb7518aa74a308f645b
= <=>
2024-01-21 21:37:09 +0300

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Grid
       templateAreas={{
-        base: `"header" "main"`,
-        lg: `"header header" "nav main"`,
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
       }}
       color="blackAlpha.700"
       fontWeight="bold"
     >
-      <GridItem pl="2" bg="orange.300" area={'header'}>
-        Header
+      <GridItem p="2" area={'nav'}>
+        <NavBar />
       </GridItem>
-      <Show above='lg'>
-        <GridItem pl="2" bg="pink.300" area={'nav'}>
-          Nav
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={'aside'}>
+          Side Bar
         </GridItem>
       </Show>
       <GridItem pl="2" bg="green.300" area={'main'}>
