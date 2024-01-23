496fe4dca5e1e89d76d81d86f341609a54456045
= <=>
2024-01-23 19:34:41 +0300

    platform icons added

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -29,7 +36,7 @@ const useGames = () => {
         setGames(res.data.results);
       } catch (err) {
         if (err instanceof CanceledError) return;
-        setErrors(err.message);
+        if (err instanceof Error) setErrors(err.message);
       }
     };
 
