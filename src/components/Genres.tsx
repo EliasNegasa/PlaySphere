d75ca32d0290c606a65e4685971fec2dd2ae5f13
= <=>
2024-01-29 15:55:45 +0300

    displaying genres

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,15 +1,27 @@
+import { HStack, Image, Stack, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getOptimizedImage from '../utils/image-url';
 
 const Genres = () => {
   const { data, errors } = useGenres();
   return (
     <>
       {errors && <p>{errors}</p>}
-      <ul>
+      <Stack p="2">
         {data.map((genre) => (
-          <li>{genre.name}</li>
+          <HStack key={genre.slug}>
+            <Image
+              src={getOptimizedImage(genre.image_background)}
+              alt={genre.name}
+              borderRadius="lg"
+              objectFit="cover"
+              height="40px"
+              width="40px"
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
         ))}
-      </ul>
+      </Stack>
     </>
   );
 };
--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -3,6 +3,7 @@ import useData from './useData';
 export interface Genres {
   name: string;
   slug: string;
+  image_background: string;
 }
 
 const useGenres = () => useData<Genres>('/genres');
