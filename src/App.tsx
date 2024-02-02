b619780ad848d6e812281ad3f010bf2ce7f55eed
= <=>
2024-02-02 16:45:12 +0300

    dynamic heading added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genres as IGenres } from './hooks/useGenres';
 import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './hooks/useGames';
 import SortSelector from './components/SortSelector';
+import GameHeading from './components/GameHeading';
 
 export interface GameQuery {
   genre: IGenres | null;
@@ -58,6 +59,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem p="2" area={'main'}>
+        <GameHeading gameQuery={gameQuery} />
         <HStack>
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
